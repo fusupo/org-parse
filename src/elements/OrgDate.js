ac53b91a9cf5a892f6ee2d53f03956a1d6aca2b8
fusupo <fusupo@gmail.com>
2017-10-30 18:14:39 -0700

    completes basic OrdDate pars/serialization features with tests

--- src/elements/OrgDate.js
+++ src/elements/OrgDate.js
@@ -1,16 +1,33 @@
-const padStart = require('../../utils').padStart;
+const padStartMaybe = require('../../utils').padStartMaybe;
 const moment = require('moment');
 
 class OrgDate {
-  static parse(src) {
-    // if (src instanceof Date) {
-    //   return OrgTimestamp.parseDate(src);
-    // } else if (typeof src === 'string') {
-    //   return OrgTimestamp.parseStr(src);
-    // }
+  static parse(dateStr) {
+    // assuming well formed dateStr
+    const ret = {
+      yyyy: '',
+      mm: '',
+      dd: '',
+      dayName: ''
+    };
+    const parts = dateStr.split(' ');
+    const [yyyy, mm, dd] = parts[0].split('-');
+    ret.yyyy = parseInt(yyyy);
+    ret.mm = parseInt(mm);
+    ret.dd = parseInt(dd);
+    ret.dayName = parts[1];
+    return ret;
   }
 
-  static serialize(timestamp) {}
+  static serialize(dateObj) {
+    // !! assuming well formed dateObj
+    let { yyyy, mm, dd, dayName } = dateObj;
+    return `${yyyy}-${padStartMaybe(mm.toString(), 2, '0')}-${padStartMaybe(
+      dd.toString(),
+      2,
+      '0'
+    )} ${dayName}`;
+  }
 }
 
 module.exports = OrgDate;
--- src/elements/OrgDate.test.js
+++ src/elements/OrgDate.test.js
@@ -9,14 +9,45 @@ describe('has static method "parse"', () => {
   });
 });
 
-describe('active timestamp', () => {
+describe('parsing', () => {
   test('parses date from string', () => {
-    const ts = '2017-10-30 Mon';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const dateStr = '2017-10-30 Mon';
+    expect(OrgDate.parse(dateStr)).toEqual({
       yyyy: 2017,
       mm: 10,
       dd: 30,
       dayName: 'Mon'
     });
+  });
+});
+
+describe('has static method "serialize"', () => {
+  test('static attribute serialize exists', () => {
+    expect(OrgDate.serialize).toBeDefined();
+  });
+  test('static attribute parse is a function', () => {
+    expect(OrgDate.serialize).toBeInstanceOf(Function);
+  });
+});
+
+describe('parsing', () => {
+  test('parses date from string', () => {
+    const dateObj = {
+      yyyy: 2017,
+      mm: 10,
+      dd: 30,
+      dayName: 'Mon'
+    };
+    expect(OrgDate.serialize(dateObj)).toBe('2017-10-30 Mon');
+  });
+
+  test('parses date from string and pads zeroes where necessary', () => {
+    const dateObj = {
+      yyyy: 2017,
+      mm: 1,
+      dd: 3,
+      dayName: 'Mon'
+    };
+    expect(OrgDate.serialize(dateObj)).toBe('2017-01-03 Mon');
   });
 });
