fb614b2053f873a38a57c1abfd5660127adfaa62
fusupo <fusupo@gmail.com>
2017-11-01 17:07:03 -0700

    DRY's OrgTimestamp tests

--- src/elements/OrgTimestamp.test.js
+++ src/elements/OrgTimestamp.test.js
@@ -1,5 +1,27 @@
 const OrgTimestamp = require('./OrgTimestamp');
 
+const gimmeTimestampObj = inputObj => {
+  const baseObj = {
+    type: null,
+    date: null,
+    dateStart: null,
+    dateEnd: null,
+    time: null,
+    timeStart: null,
+    timeEnd: null,
+    repeat: null,
+    repeatStart: null,
+    repeatEnd: null,
+    delay: null,
+    delayStart: null,
+    delayEnd: null
+  };
+  Object.keys(inputObj).forEach(k => {
+    baseObj[k] = inputObj[k];
+  });
+  return baseObj;
+};
+
 describe('has static method "parse"', () => {
   test('static attribute parse exists', () => {
     expect(OrgTimestamp.parse).toBeDefined();
@@ -12,30 +34,21 @@ describe('has static method "parse"', () => {
 describe('simple active timestamp', () => {
   test('parses active timestamp without time or reapeater-or-delay', () => {
     const ts = '<2017-10-30 Mon>';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const resultObj = gimmeTimestampObj({
       type: OrgTimestamp.ACTIVE,
       date: {
         yyyy: 2017,
         mm: 10,
         dd: 30,
         dayName: 'Mon'
-      },
-      dateStart: null,
-      dateEnd: null,
-      time: null,
-      timeStart: null,
-      timeEnd: null,
-      repeat: null,
-      repeatStart: null,
-      repeatEnd: null,
-      delay: null,
-      delayStart: null,
-      delayEnd: null
+      }
     });
+    expect(OrgTimestamp.parse(ts)).toEqual(resultObj);
   });
+
   test('parses active timestamp with time but no repeater-or-delay', () => {
     const ts = '<2017-10-31 Tue 20:45>';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const resultObj = gimmeTimestampObj({
       type: OrgTimestamp.ACTIVE,
       date: {
         yyyy: 2017,
@@ -43,25 +56,17 @@ describe('simple active timestamp', () => {
         dd: 31,
         dayName: 'Tue'
       },
-      dateStart: null,
-      dateEnd: null,
       time: {
         hh: 20,
         mm: 45
-      },
-      timeStart: null,
-      timeEnd: null,
-      repeat: null,
-      repeatStart: null,
-      repeatEnd: null,
-      delay: null,
-      delayStart: null,
-      delayEnd: null
+      }
     });
+    expect(OrgTimestamp.parse(ts)).toEqual(resultObj);
   });
+
   test('parses active timestamp range B', () => {
     const ts = '<2017-10-31 Tue 20:45-21:37>';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const resultObj = gimmeTimestampObj({
       type: OrgTimestamp.ACTIVE_RANGE_B,
       date: {
         yyyy: 2017,
@@ -69,9 +74,6 @@ describe('simple active timestamp', () => {
         dd: 31,
         dayName: 'Tue'
       },
-      dateStart: null,
-      dateEnd: null,
-      time: null,
       timeStart: {
         hh: 20,
         mm: 45
@@ -79,44 +81,29 @@ describe('simple active timestamp', () => {
       timeEnd: {
         hh: 21,
         mm: 37
-      },
-      repeat: null,
-      repeatStart: null,
-      repeatEnd: null,
-      delay: null,
-      delayStart: null,
-      delayEnd: null
+      }
     });
+    expect(OrgTimestamp.parse(ts)).toEqual(resultObj);
   });
 });
 
 describe('simple inactive timestamp', () => {
   test('parses inactive timestamp without time or reapeater-or-delay', () => {
     const ts = '[2017-10-30 Mon]';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const resultObj = gimmeTimestampObj({
       type: OrgTimestamp.INACTIVE,
       date: {
         yyyy: 2017,
         mm: 10,
         dd: 30,
         dayName: 'Mon'
-      },
-      dateStart: null,
-      dateEnd: null,
-      time: null,
-      timeStart: null,
-      timeEnd: null,
-      repeat: null,
-      repeatStart: null,
-      repeatEnd: null,
-      delay: null,
-      delayStart: null,
-      delayEnd: null
+      }
     });
+    expect(OrgTimestamp.parse(ts)).toEqual(resultObj);
   });
   test('parses inactive timestamp with time but no repeater-or-delay', () => {
     const ts = '[2017-10-31 Tue 20:45]';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const resultObj = gimmeTimestampObj({
       type: OrgTimestamp.INACTIVE,
       date: {
         yyyy: 2017,
@@ -124,25 +111,16 @@ describe('simple inactive timestamp', () => {
         dd: 31,
         dayName: 'Tue'
       },
-      dateStart: null,
-      dateEnd: null,
       time: {
         hh: 20,
         mm: 45
-      },
-      timeStart: null,
-      timeEnd: null,
-      repeat: null,
-      repeatStart: null,
-      repeatEnd: null,
-      delay: null,
-      delayStart: null,
-      delayEnd: null
+      }
     });
+    expect(OrgTimestamp.parse(ts)).toEqual(resultObj);
   });
   test('parses inactive timestamp range B', () => {
     const ts = '[2017-10-31 Tue 20:45-21:37]';
-    expect(OrgTimestamp.parse(ts)).toEqual({
+    const resultObj = gimmeTimestampObj({
       type: OrgTimestamp.INACTIVE_RANGE_B,
       date: {
         yyyy: 2017,
@@ -150,9 +128,6 @@ describe('simple inactive timestamp', () => {
         dd: 31,
         dayName: 'Tue'
       },
-      dateStart: null,
-      dateEnd: null,
-      time: null,
       timeStart: {
         hh: 20,
         mm: 45
@@ -160,14 +135,9 @@ describe('simple inactive timestamp', () => {
       timeEnd: {
         hh: 21,
         mm: 37
-      },
-      repeat: null,
-      repeatStart: null,
-      repeatEnd: null,
-      delay: null,
-      delayStart: null,
-      delayEnd: null
+      }
     });
+    expect(OrgTimestamp.parse(ts)).toEqual(resultObj);
   });
 });
 
