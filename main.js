2260adb0bbe62d93dd4e334bcf972eecdd7d8f6f
fusupo <fusupo@gmail.com>
2017-11-27 11:00:51 -0800

    updates main.js

--- main.js
+++ main.js
@@ -1,86 +1,38 @@
-const OrgDrawer = require('./org-drawer');
-const OrgTree = require('./org-tree');
-const OrgNode = require('./org-node');
-const OrgHeadline = require('./org-headline');
-const OrgTimestamp = require('./org-timestamp');
-const nodeHasActiveTimeStamp_p = require('./utils').nodeHasActiveTimeStamp_p;
-const activeTimeStampFromNode = require('./utils').activeTimeStampFromNode;
+const OrgDocument = require('./src/OrgDocument');
+const OrgHeadline = require('./src/OrgHeadline');
+const OrgSection = require('./src/OrgSection.js');
 
-const createNodeMaybe = srcStr => {
-  if (srcStr[0] === '*') {
-    let node = OrgNode.parse(srcStr);
-    return node;
-  } else {
-    return false;
-  }
+const OrgPropDrawer = require('./src/greater-elements/OrgPropDrawer.js');
+
+const OrgKeyword = require('./src/elements/OrgKeyword');
+const OrgPlanning = require('./src/elements/OrgPlanning');
+
+const OrgTimestamp = require('./src/objects/OrgTimestamp.js');
+
+//--------------------------------------
+
+const parseOrg = (srcStr, store) => {
+  let doc = OrgDocument.parse(srcStr, store);
+  return doc;
 };
 
-const parseTree = (parentTree, nodes, nodeIDs) => {
-  let idx = 0;
-  const innerFunc = pt => {
-    const currID = nodeIDs[idx];
-    let currNode = nodes[currID];
-    let currTree = OrgTree.new(currID);
-    idx++;
-    pt.children.push(currTree);
-    while (
-      idx < nodeIDs.length &&
-      nodes[nodeIDs[idx]].headline.level === currNode.headline.level + 1
-    ) {
-      innerFunc(currTree);
-    }
-    if (
-      idx < nodeIDs.length &&
-      currNode.headline.level === nodes[nodeIDs[idx]].headline.level
-    ) {
-      innerFunc(pt);
-    }
-  };
-  innerFunc(parentTree);
-};
-
-const parseOrg = srcStr =>
-  new Promise((resolve, reject) => {
-    let nodesSrc = srcStr.split(/\n(?=\*)/gm);
-    let settings = [];
-    let nodes = {};
-    let tree = OrgTree.new('root');
-    while (nodesSrc[0].startsWith('#+')) {
-      settings.push(nodesSrc[0]);
-      nodesSrc.shift();
-    }
-    for (let idx in nodesSrc) {
-      let node = createNodeMaybe(nodesSrc[idx]);
-      if (node) {
-        nodes[node.id] = node;
-      }
-    }
-    parseTree(tree, nodes, Object.keys(nodes));
-    //tree.header = nodesSrc[0]; //super hacky to save to document header!!!
-    resolve({ nodes, tree, settings });
-  });
-
-const serialize = (nodes, tree, settings) => {
+const serialize = orgDocument => {
   let ret = '';
-  if (settings !== null && settings.length > 0) {
-    ret += settings.join('\n');
-    ret += '\n';
-  }
-  ret += OrgTree.serialize(tree, nodes);
   return ret;
 };
 
-let utils = {
-  nodeHasActiveTimeStamp_p,
-  activeTimeStampFromNode
-};
-
-module.exports.parseOrg = parseOrg;
+module.exports.parse = parseOrg;
 module.exports.serialize = serialize;
-module.exports.utils = utils;
+
+//--------------------
+
+module.exports.OrgDocument = OrgDocument;
+module.exports.OrgHeadline = OrgHeadline;
+module.exports.OrgSection = OrgSection;
+
+module.exports.OrgPropDrawer = OrgPropDrawer;
+
+module.exports.OrgKeyword = OrgKeyword;
+module.exports.OrgPlanning = OrgPlanning;
 
 module.exports.OrgTimestamp = OrgTimestamp;
-module.exports.OrgDrawer = OrgDrawer;
-module.exports.OrgTree = OrgTree;
-module.exports.OrgNode = OrgNode;
-module.exports.OrgHeadline = OrgHeadline;
--- main.test.js
+++ main.test.js
@@ -1,20 +1,23 @@
-const orgParse = require('./main');
-const parseOrg = orgParse.parseOrg;
-const serializeOrg = orgParse.serialize;
-const OrgTree = require('./org-tree');
+const OrgParser = require('./main');
 const fs = require('fs');
 
-const testOrgStr = fs.readFileSync('./projects.org').toString();
+const testOrgStr = fs.readFileSync('./test.org').toString();
 
 test('is method', () => {
-  expect(parseOrg).toBeInstanceOf(Function);
+  expect(OrgParser.parse).toBeInstanceOf(Function);
 });
 
-test('does something', () => {
-  parseOrg(testOrgStr).then(res => {
-    expect(res).toBeInstanceOf(Object);
-    expect(res.nodes).toBeInstanceOf(Object);
-    expect(res.tree).toBeInstanceOf(Object);
-    serializeOrg(res.nodes, res.tree, res.settings);
-  });
-});
+// test('does something', () => {
+//   parseOrg(testOrgStr).then(res => {
+//     expect(res).toBeInstanceOf(Object);
+//     expect(res.nodes).toBeInstanceOf(Object);
+//     expect(res.tree).toBeInstanceOf(Object);
+//     serializeOrg(res.nodes, res.tree, res.settings);
+//   });
+// });
+
+let store = {};
+let foo = OrgParser.parse(testOrgStr, store);
+
+//console.log(foo);
+console.log(JSON.stringify(store, null, 1));
