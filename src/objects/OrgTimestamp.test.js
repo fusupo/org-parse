cebc0ef37a5bd74124a995de78ac2668b3cd527f
fusupo <fusupo@gmail.com>
2017-12-28 17:36:58 -0800

    Adds basic inactive timestamp tests

--- src/objects/OrgTimestamp.test.js
+++ src/objects/OrgTimestamp.test.js
@@ -94,35 +94,65 @@ describe('parses active timestamp', () => {
   });
 });
 
-// describe('simple inactive timestamp', () => {
-//   test('parses inactive timestamp without time or reapeater-or-delay', () => {
-//     const timestampStr = '[2017-10-30 Mon]';
-//     const timestampObj = gimmeTimestampObj({
-//       type: OrgTimestamp.INACTIVE,
-//       date: new OrgDate(2017, 10, 30, 'Mon')
-//     });
-//     expect(OrgTimestamp.parse(timestampStr)).toEqual(timestampObj);
-//   });
-//   test('parses inactive timestamp with time but no repeater-or-delay', () => {
-//     const timestampStr = '[2017-10-31 Tue 20:45]';
-//     const timestampObj = gimmeTimestampObj({
-//       type: OrgTimestamp.INACTIVE,
-//       date: new OrgDate(2017, 10, 31, 'Tue'),
-//       time: new OrgTime(20, 45)
-//     });
-//     expect(OrgTimestamp.parse(timestampStr)).toEqual(timestampObj);
-//   });
-//   test('parses inactive timestamp range B', () => {
-//     const timestampStr = '[2017-10-31 Tue 20:45-21:37]';
-//     const timestampObj = gimmeTimestampObj({
-//       type: OrgTimestamp.INACTIVE_RANGE_B,
-//       date: new OrgDate(2017, 10, 31, 'Tue'),
-//       timeStart: new OrgTime(20, 45),
-//       timeEnd: new OrgTime(21, 37)
-//     });
-//     expect(OrgTimestamp.parse(timestampStr)).toEqual(timestampObj);
-//   });
-// });
+describe('parses inactive timestamp', () => {
+  test('parses inactive timestamp without time or reapeater-or-delay', () => {
+    const timestampStr = '[2017-10-30 Mon]';
+    const store = {};
+    const timestampObj = gimmeTimestampObj({
+      type: OrgTimestamp.INACTIVE,
+      date: OrgDate.parse('2017-10-30 Mon', store).id
+    });
+    const parsedObj = OrgTimestamp.parse(timestampStr, store);
+    expect(timestampObj.type).toBe(parsedObj.type);
+    expect(timestampObj.date).toBe(parsedObj.date);
+  });
+
+  test('parses inactive timestamp with time but no repeater-or-delay', () => {
+    const timestampStr = '[2017-10-31 Tue 20:45]';
+    const store = {};
+    const timestampObj = gimmeTimestampObj({
+      type: OrgTimestamp.INACTIVE,
+      date: OrgDate.parse('2017-10-31 Tue', store).id,
+      time: OrgTime.parse('20:45', store).id
+    });
+    const parsedObj = OrgTimestamp.parse(timestampStr, store);
+    expect(parsedObj.type).toBe(timestampObj.type);
+    expect(parsedObj.date).toBe(timestampObj.date);
+    expect(parsedObj.time).toBe(timestampObj.time);
+  });
+
+  test('parses inactive timestamp range B', () => {
+    const timestampStr = '[2017-10-31 Tue 20:45-21:37]';
+    const store = {};
+    const timestampObj = gimmeTimestampObj({
+      type: OrgTimestamp.INACTIVE_RANGE_B,
+      date: OrgDate.parse('2017-10-31 Tue', store).id,
+      timeStart: OrgTime.parse('20:45', store).id,
+      timeEnd: OrgTime.parse('21:37', store).id
+    });
+    const parsedObj = OrgTimestamp.parse(timestampStr, store);
+    expect(parsedObj.type).toBe(timestampObj.type);
+    expect(parsedObj.date).toBe(timestampObj.date);
+    expect(parsedObj.timeStart).toBe(timestampObj.timeStart);
+    expect(parsedObj.timeEnd).toBe(timestampObj.timeEnd);
+  });
+
+  test('parses inactive timestamp with repeater', () => {
+    const timestampStr = '[2017-05-06 Sat 13:42 .+1w]';
+    const store = {};
+    const timestampObj = gimmeTimestampObj({
+      type: OrgTimestamp.INACTIVE,
+      date: OrgDate.parse('2017-05-06 Sat', store).id,
+      time: OrgTime.parse('13:42', store).id,
+      repeat: '.+1w'
+    });
+    const parsedObj = OrgTimestamp.parse(timestampStr, store);
+    expect(parsedObj.type).toEqual(timestampObj.type);
+    expect(parsedObj.date).toEqual(timestampObj.date);
+    expect(parsedObj.time).toEqual(timestampObj.time);
+    expect(parsedObj.repeat).toEqual(timestampObj.repeat);
+  });
+});
 
 // describe('has static method "serialize"', () => {
 //   test('static attribute serialize exists', () => {
